e8eff32cc223e7e6098d503796a21c5afbc37d62
niku <niku@niku.name>
2011-07-29 00:32:17 +0900

    add relase plan

--- spec/nwiki_spec.rb
+++ spec/nwiki_spec.rb
@@ -9,4 +9,25 @@ describe Nwiki do
     end
     it{ should be_ok }
   end
+
+  # v0.1 now!
+  context "when get data"
+  context "given specify rootpath of data"
+  context "when get data does not exist"
+
+  # v0.2
+  context "when access summary"
+  context "when access feed"
+
+  # v0.3
+  context "given get some type"
+  context "given get org-mode data"
+
+  # TODO
+  context "given get data include image path"
+  context "when search data"
+  context "when search tag"
+  context "given specify css"
+  context "when post data"
+  context "given add plugin"
 end
