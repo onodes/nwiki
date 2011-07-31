686af541efb93ab8fe710ca5c6aefc719a39c47f
niku <niku@niku.name>
2011-07-31 13:10:55 +0900

    refactoring. use FileTest.

--- lib/nwiki.rb
+++ lib/nwiki.rb
@@ -4,17 +4,17 @@ require_relative "nwiki/version"
 module Nwiki
   ROOT_PATH = "."
   def self.call(env)
-    return [403, {"Content-Type" => "text/plain"}, ["forbidden."]] if env["PATH_INFO"].include? ".."
-    begin
-      [200, {"Content-Type" => "text/plain"}, [File.read(ROOT_PATH + env["PATH_INFO"])]]
-    rescue => ex
-      if ex.message =~ /^No such file or directory/
-        [404, {"Content-Type" => "text/plain"}, ["not found."]]
+    path = env["PATH_INFO"].gsub(/^\//){ '' }
+    return [403, {"Content-Type" => "text/plain"}, ["forbidden."]] if path.include? ".."
+    Dir.chdir(ROOT_PATH) do
+      case
+      when FileTest.file?(path)
+        [200, {"Content-Type" => "text/plain"}, [File.read(path)]]
+      when FileTest.directory?(path)
+        search_dir = path + "/**/*"
+        [200, {"Content-Type" => "text/plain"}, [Dir.glob(search_dir).select{ |path| File.file?(path) }.sort.join("\n")]]
       else
-        Dir.chdir(ROOT_PATH) do
-          search_dir = "#{env['PATH_INFO'].gsub(/^\//){ '' }}/**/*"
-          [200, {"Content-Type" => "text/plain"}, [Dir.glob(search_dir).select{ |path| File.file?(path) }.sort.join("\n")]]
-        end
+        [404, {"Content-Type" => "text/plain"}, ["not found."]]
       end
     end
   end
