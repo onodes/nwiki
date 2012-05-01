846d8dd8ff0d96463aee7855a8e9e4881eb7ac9c
niku <niku@niku.name>
2012-05-01 17:28:40 +0900

    fix indent

--- spec/nwiki/core/wiki_spec.rb
+++ spec/nwiki/core/wiki_spec.rb
@@ -7,22 +7,22 @@ module Nwiki
         subject { described_class.new('spec/examples/empty.git') }
       end
       describe '.new' do
-      context 'when uses empty.git' do
-      include_context 'when uses empty.git'
-      it { expect { subject }.not_to raise_error }
-      end
+        context 'when uses empty.git' do
+          include_context 'when uses empty.git'
+          it { expect { subject }.not_to raise_error }
+        end
       end
       describe '#pages' do
-      context 'when uses empty.git' do
-      include_context 'when uses empty.git'
-      its(:pages) { should eq [] }
-      end
+        context 'when uses empty.git' do
+          include_context 'when uses empty.git'
+          its(:pages) { should eq [] }
+        end
       end
       describe '#page' do
-      context 'when uses empty.git' do
-      include_context 'when uses empty.git'
-      it { subject.page('foo').should be_nil }
-      end
+        context 'when uses empty.git' do
+          include_context 'when uses empty.git'
+          it { subject.page('foo').should be_nil }
+        end
       end
     end
   end
