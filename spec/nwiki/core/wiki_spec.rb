4c5578fdaa5b5b727e4a5ba39f9fee04a1509f4f
niku <niku@niku.name>
2012-05-01 17:28:40 +0900

    wrap with shared context

--- spec/nwiki/core/wiki_spec.rb
+++ spec/nwiki/core/wiki_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 module Nwiki
   module Core
     describe Wiki do
+      shared_context 'when uses empty.git' do
       subject { described_class.new('spec/examples/empty.git') }
+      end
+      include_context 'when uses empty.git'
       it { expect { subject }.not_to raise_error }
       its(:pages) { should eq [] }
       it { subject.page('foo').should be_nil }
