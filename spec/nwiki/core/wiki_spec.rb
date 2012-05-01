e11081cd592fffa298f2bafc6e25aaf96d5948c5
niku <niku@niku.name>
2012-05-01 17:28:40 +0900

    separate spec into methods

--- spec/nwiki/core/wiki_spec.rb
+++ spec/nwiki/core/wiki_spec.rb
@@ -6,10 +6,24 @@ module Nwiki
       shared_context 'when uses empty.git' do
         subject { described_class.new('spec/examples/empty.git') }
       end
+      describe '.new' do
+      context 'when uses empty.git' do
       include_context 'when uses empty.git'
       it { expect { subject }.not_to raise_error }
+      end
+      end
+      describe '#pages' do
+      context 'when uses empty.git' do
+      include_context 'when uses empty.git'
       its(:pages) { should eq [] }
+      end
+      end
+      describe '#page' do
+      context 'when uses empty.git' do
+      include_context 'when uses empty.git'
       it { subject.page('foo').should be_nil }
+      end
+      end
     end
   end
 end
