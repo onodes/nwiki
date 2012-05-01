91d7ea0278d417fea63abd03308442c991bbdeb6
niku <niku@niku.name>
2012-05-01 17:28:40 +0900

    add spec of Page#filename

---
+++ spec/nwiki/core/page_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module Nwiki
+  module Core
+    describe Page do
+      subject { described_class.new(BlobEntry.new(sha, path)) }
+      let(:sha) { 'bda3255f29e545c6a9fa73f2fc8aa40b2f531ce3' }
+      let(:path) { 'foo.org' }
+
+      describe '#filename' do
+        its(:filename) { should eq path }
+      end
+    end
+  end
+end
